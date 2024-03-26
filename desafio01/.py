0e7a450443de8f1c515362e1343bd0ac58692608
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-03-26 10:56:48 -0300

    01

---
+++ desafio01/.py
@@ -0,0 +1,27 @@
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+pessoas = []
+
+while True:
+    nome = input("Digite o seu nome: (ou digite 'fim' se deseja encerrar.)")
+    if nome == 'fim' :
+        break
+    peso = int(input("Digite o seu peso:"))
+    altura = int(input("Digite a sua altura:"))
+    pessoas.append({'nome': nome, 'peso': peso, 'altura': altura })
+   
+    
+
+for pessoa in pessoas:
+    pessoa['imc'] = calcular_imc(pessoa['peso'], pessoa['altura'])
+
+import statistics
+imcs = [pessoa['imc'] for pessoa in pessoas]
+media_imc = statistics.mean(imcs)
+mediana_imc = statistics.median(imcs)
+desvio_padrao = statistics.pstdev(imcs)
+
+print(f"Média do IMC: {media_imc}")
+print(f"Mediana do IMC: {mediana_imc}")
+print(f"Desvio padrão do IMC: {desvio_padrao}")
